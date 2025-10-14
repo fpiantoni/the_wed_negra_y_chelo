05a166041a72c06d49da0a1c694c59d89b036bb1
fpiantoni <fran@piantoni.com.ar>
2025-10-13 21:06:29 -0300

    added: fonts and other changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
@@ -22,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+      <body className="font-poppins">
         {children}
         <Analytics />
       </body>
