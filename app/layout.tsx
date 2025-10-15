e2eb1f37905e64b01ee155291cada0e972d72423
fpiantoni <fran@piantoni.com.ar>
2025-10-15 19:50:59 -0300

    added: logo and changed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,9 @@ export const metadata: Metadata = {
   title: 'The Wed N&C',
   description: 'Negra & Chelo Wedding - Celebrá nuestro amor con nosotros',
   icons: {
-    icon: [
-      { url: '/red_logo.svg', sizes: 'any', type: 'image/svg+xml' },
-      { url: '/red_logo_favicon.jpeg', sizes: '32x32', type: 'image/jpeg' },
-      { url: '/red_logo_favicon.jpeg', sizes: '16x16', type: 'image/jpeg' }
-    ],
-    shortcut: '/red_logo.svg',
-    apple: '/red_logo_favicon.jpeg',
+    icon: '/red_logo_png.png',
+    shortcut: '/red_logo_png.png',
+    apple: '/red_logo_png.png',
   },
 }
 
@@ -24,13 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" type="image/svg+xml" href="/red_logo.svg?v=2" />
-        <link rel="icon" type="image/jpeg" sizes="32x32" href="/red_logo_favicon.jpeg?v=2" />
-        <link rel="icon" type="image/jpeg" sizes="16x16" href="/red_logo_favicon.jpeg?v=2" />
-        <link rel="shortcut icon" href="/red_logo.svg?v=2" />
-        <link rel="apple-touch-icon" href="/red_logo_favicon.jpeg?v=2" />
-      </head>
       <body className="font-poppins">
         {children}
         <Analytics />
